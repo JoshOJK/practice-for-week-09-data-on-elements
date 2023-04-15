6c55fed077868a6077583b553975fac1fa142adc
joshjk <jokrienke@comcast.net>
2023-04-15 12:15:33 -0700

    completed

--- data.js
+++ data.js
@@ -1,1 +1,22 @@
-// Your code here
+let list = document.getElementById("shopping-list");
+let button = document.getElementById("add");
+let select = document.getElementById("type");
+
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let attributeValue = select.options[select.selectedIndex].getAttribute("value");
+
+    let userInput = document.getElementById("name").value
+
+    const listItem = document.createElement('li')
+
+    listItem.setAttribute("data-type", attributeValue)
+
+    let text = document.createTextNode(userInput)
+
+    listItem.appendChild(text)
+    list.appendChild(listItem)
+
+    form.reset();
+})
